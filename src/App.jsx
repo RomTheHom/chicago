f4bb7de5b034215c60846a89b8361b8f35422922
Robel Girmay <robel.mattias4@gmail.com>
2024-04-17 11:13:59 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      Welcome to Chicago, the second city aka the windy city aka the windy apple
+    </>
+  )
+}
+
+export default App
